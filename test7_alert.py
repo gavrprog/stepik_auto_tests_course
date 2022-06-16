2ed31f74526e71b7296632c023afdfd1e357f812
Michael Gavrilenko <gavr.inbox@gmail.com>
2022-06-16 12:55:41 +0200

    работа с окнами типа Alert

---
+++ test7_alert.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return math.log(abs(12 * math.sin(x)))
+
+try:
+    link = " http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    browser.find_element(By.TAG_NAME, 'button').click()
+    browser.switch_to.alert.accept()
+    nummer = browser.find_element(By.ID, 'input_value').text
+    answer = calc(int(nummer))
+    browser.find_element(By.ID, 'answer').send_keys(str(answer))
+
+    browser.find_element(By.CLASS_NAME, 'btn').click()
+
+    answer = browser.switch_to.alert.text
+    print(answer)
+    browser.switch_to.alert.accept()
+
+finally:
+    
+    time.sleep(5)
+    browser.quit()
